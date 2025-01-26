34e811d1804c6cc771aa9d651675509d48063661
Joseph Pennella <josephpennella@dhcp-10-105-212-222.wireless.northwestern.private>
2025-01-26 16:02:39 -0600

    first_files

---
+++ personal_website/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='header'>
+      <h1>Hello. My Name is Joseph Pennella,</h1>
+      <h2>A Computer Science Major at Northwestern University.</h2>
+    </div>
+    <div className='interests'>
+      <a href='https://open.spotify.com/playlist/1RWI4T07ZaB124qd9LMDhj?si=ERgZuSHIRPmyqQ9YnBL_hg'>Spotify</a>
+    </div>
+    <div className="links">
+
+    </div>
+    <div className="projects">
+
+    </div>
+    </>
+  )
+}
+
+export default App
